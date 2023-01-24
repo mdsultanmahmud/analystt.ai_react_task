cfa2ae8766b0eaded859b4c0c665cb95e5374bc6
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2023-01-24 21:00:13 +0600

    design home page and done project

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,14 +1,35 @@
 import React, { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Triangle } from 'react-loader-spinner';
 import './Home.css'
 const Home = () => {
     const [users, setUsers] = useState({})
-
+    const [loader, setLoader] = useState(true)
     useEffect(() => {
+        setLoader(true)
         fetch('https://jsonplaceholder.typicode.com/users')
+
             .then(res => res.json())
-            .then(data => setUsers(data))
+            .then(data => {
+                setUsers(data)
+                if (data) {
+                    setLoader(false)
+                }
+            })
     }, [])
+    if (loader) {
+        return <div className='loader-container'>
+            <Triangle
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="triangle-loading"
+                wrapperStyle={{}}
+                wrapperClassName=""
+                visible={true}
+            />
+        </div>
+    }
     return (
         <div>
             <h1 className='title'>All Users Information</h1>
@@ -16,23 +37,19 @@ const Home = () => {
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>User Name</th>
                         <th>Email</th>
                         <th>Website</th>
-                        <th>Company</th>
                         <th>About</th>
                     </tr>
                 </thead>
                 <tbody>
-                    { users.length > 0 &&
+                    {users.length > 0 &&
                         users.map(user => {
-                            const { id, name, username, email, website, company } = user
+                            const { id, name, email, website, company } = user
                             return <tr key={id}>
                                 <td>{name}</td>
-                                <td>{username}</td>
                                 <td>{email}</td>
                                 <td>{website}</td>
-                                <td>{company?.name}</td>
                                 <td><Link to={`/home/${id}`}><button className='details-btn'>Details</button></Link></td>
                             </tr>
                         })
