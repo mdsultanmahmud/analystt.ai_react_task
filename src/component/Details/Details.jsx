862e160eacdb2bb126cd13a869332b74942175c5
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2023-01-24 20:17:13 +0600

    show information for specifiq user

--- src/component/Details/Details.jsx
+++ src/component/Details/Details.jsx
@@ -3,9 +3,28 @@ import { useLoaderData } from 'react-router-dom';
 
 const Details = () => {
     const userDetails = useLoaderData()
+    const { name, username, email, address, company, website, phone } = userDetails
+    console.log(userDetails)
     return (
         <div>
-            <h1>show details information about {userDetails.name}</h1>
+            <h1>show details information about {name}</h1>
+            <div>
+                <p><strong>User Name:</strong> {username}</p>
+                <p><strong>Email: </strong>{email}</p>
+                <p><strong>Website: </strong>{website}</p>
+                <p><strong>Phone: </strong>{phone}</p>
+                <p><strong>Email: </strong>{email}</p>
+                <p><strong>Company Name: </strong>{company.name}</p>
+                <address>
+                    <p><strong>City:</strong>{address.city}</p>
+                    <p><strong>Street:</strong>{address.street}</p>
+                    <p><strong>Zipcode:</strong>{address.zipcode}</p>
+                    <p><strong>GEO:</strong>
+                        <span>Lat: {address.geo.lat}</span>
+                        <span>Lng: {address.geo.lng}</span>
+                    </p>
+                </address>
+            </div>
         </div>
     );
 };
